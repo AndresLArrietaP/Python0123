02815b51051ea68f12d9972bbdad8ce6986fb8b5
AndyArrieta <andrestransformers@hotmail.com>
2023-01-16 02:13:08 -0500

    PRAC 2 v.1.2 E1 fin

--- Python0123/ejer1.py
+++ Python0123/ejer1.py
@@ -31,12 +31,39 @@ if(opc!='A' or opc!='B' or opc!='C'):
         n=int(input('Ingrese cantidad de números: '))
         ##Creo lista
         lis=[]
-        for i,num in enumerate(lis):
-
-            print('a')
+        for num in range(1,n+1):
+            lis.append(num)
+        #print(lis)
+        print('\n')
+        for num in lis:
+            prim=True
+            if num % 2 == 0:
+                prim=False
+            if prim:
+                print(num,' no es multiplo de 2')
+            else:
+                print(num,' es multiplo de 2')
     elif opc=='C':
-        print('a')
+        n=int(input('Ingrese cantidad de usuarios: '))
+        ##Creo lista general
+        usuarios=[]
+        for item in range(1, n+1):
+            #Lista por usuario
+            u=[]
+            x = input(f'Ingrese el nombre del {item} usuario: ')
+            y = int(input(f'Ingrese la edad del {item} usuario: ')) 
+            u.append(x)
+            u.append(y)
+            usuarios.append(u)
+            pass
+        ##Mayores de edad
+        print('\nUsuarios mayores de edad: ')
+        for user in usuarios:
+            if user[-1]>=18:
+                print('\nNombre: ',user[0])
+                print('Edad: ',user[-1])
+            
     else:
-        print('esta opcion no es valida')
+        print('OPCION INVÁLIDA')
 else:
-    print('elija una opcion correcta')
+    print('OPCIÓN INVALIDA')
