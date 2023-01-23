3fb63ae28f86f0ae7262bbd4d3d4bd2fb099888e
AndyArrieta <andrestransformers@hotmail.com>
2023-01-23 02:44:15 -0500

    PC3 v1.5 EJER4

--- PC3/Python2223/tempCodeRunnerFile.py
+++ PC3/Python2223/tempCodeRunnerFile.py
@@ -1,12 +1,41 @@
-valor= int(input("\nValor a multiplicar: "))
-lim = int(input("\n¿Cuantas veces multiplicar? "))
+class Catalogo:
+    productos = [] 
+    
+    def __init__(self,productos=[]):
+        self.productos=productos
 
-def multi(valor : int ,repe : int =1  ):
-    if repe <=lim:
-        repe+=1
-        print(valor)
-        return multi(valor*2,repe)
-    else:
-        print('Resultado: ',valor)
-        print('\nFin')
-multi(valor)
+    def agregarp(self, pro):  # pro ->producto
+        self.productos.append(pro)
+
+    def mostrarp(self):
+        print('\n<<<PRODUCTOS>>>')
+        for pro in self.productos:
+            print(pro)  
+
+class Producto:
+    def __init__(self, precio, descripcion,stock):
+        self.precio = precio
+        self.descripcion = descripcion
+        self.stock = stock
+    def __str__(self):
+        return '\nPrecio: {} \nDescripción: {} \nStock: {}'.format(self.precio, self.descripcion,self.stock)
+
+msg = "\nRegistrar Precio , Descripcion breve y Stock disponible:"
+
+
+print(msg)
+
+c = Catalogo()
+
+rpta='Si'
+    #Añadir productos hasta respuesta negativa
+while rpta=='Si':
+    pre=float(input('\nPrecio del producto: '))
+    des=input('\nDescripcion: ')
+    sto=int(input('\nStock disponible: '))
+    pr = Producto(pre,des,sto)
+ 
+    c.agregarp(pr)
+    print('¿Desea agregar otro producto?')
+    rpta=input('[Si/No]: ').capitalize()
+c.mostrarp()
